5c7ee734129d918ffe40fffbc68180c6075c9935
Michelle Brant <MichelleBrant@Michelles-MacBook-Pro.local>
2016-10-12 14:42:02 -0400

    added my first part of java

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "King";
+var cardFour = "King";
+if(cardOne === cardTwo){
+	alert('You found a match!');
+}
+else if(cardThree === cardFour){
+	alert('You found another match!');
+}
