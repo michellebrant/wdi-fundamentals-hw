6d9a826b31d195b6bf2e4c344bf21775101bc5f9
Michelle Brant <MichelleBrant@Michelles-MacBook-Pro.local>
2016-10-14 11:59:28 -0400

    added java

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
-if(cardOne === cardTwo){
-	alert('You found a match!');
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "King";
+//var cardFour = "King";
+//if(cardOne === cardTwo){
+	//alert('You found a match!');
+//}
+//else if(cardThree === cardFour){
+	//alert('You found another match!');
+//}
+var createCards = function(i){
+for (i=0; i < 4; i++) {
+	var board = document.getElementById('game-board');
+	var newCardElement = document.createElement('div');
+	newCardElement.className = ('card');
+	document.getElementById('game-board').appendChild(newCardElement);
 }
-else if(cardThree === cardFour){
-	alert('You found another match!');
 }
+createCards(4);
